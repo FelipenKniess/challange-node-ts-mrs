f01122b7c3aafc25fb24f4a65e8cd873781e9f71
FelipenKniess <felipe.nkniess@gmail.com>
2020-12-15 18:03:48 -0300

    Aplicação finalizada

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+
+      if( type == 'outcome'){
+        const balance = this.transactionsRepository.getBalance();
+        const totalBalance = balance.total - value;
+
+        if(totalBalance < 0){
+          throw Error('should not be able to create outcome transaction without a valid balance');
+        }
+      }
+
+      return this.transactionsRepository.create({
+        title,
+        value,
+        type
+      });
   }
 }
 
